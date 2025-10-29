c8921172b27e1d1f6e04a3413339fca9e121c34e
uliannaa <152221245+ulyannaa@users.noreply.github.com>
2025-10-29 01:02:01 -0400

    declared all the variables and functions in the header file, created a node constructor, and defined the getter functions

--- Node.cpp
+++ Node.cpp
@@ -9,11 +9,44 @@
 
 using namespace std;
 
-Node::Node(int id, char state, float infectionRate): 
-    node_id(id), 
-    degree_of_separation(0), 
-    curr_state(state), 
-    time_exposed(-1.0), 
-    time_infect(-1.0), 
-    recovery_time(-1.0), 
-    infection_rate(infectionRate) {}
+Node::Node(int id, char state, float infectionRate, float incubationRate, float recoveryRate){
+    node_id = id;
+    curr_state = state;
+    infection_rate = infectionRate;
+    incubation_rate = incubationRate;
+    recovery_rate = recovery_rate;
+}
+
+int Node::getId(){
+    return node_id;
+    }
+int Node::getDegree(){
+    return neighbors.size();
+}
+int Node::getDegreeOfSeparation(){
+    return degree_of_separation;
+}
+char Node::getState(){
+    return curr_state;
+}
+char Node::getState(){
+    return curr_state;
+}
+float Node::getTimeExposed(){
+    return time_exposed;
+}
+float Node::getTimeInfected(){
+    return time_infect;
+}
+float Node::getTimeRecovered(){
+    return time_recovery;
+}
+float Node::getInfectionRate(){
+    return infection_rate;
+}
+float Node::getIncubationRate(){
+    return incubation_rate;
+}
+float Node::getRecoveryRate(){
+    return recovery_rate;
+}
--- Node.h
+++ Node.h
@@ -2,15 +2,48 @@
 using namespace std;
 
 class Node {
-public:
-    Node(int id, char state = 'S', float infectionRate = 0.0);
+private:
+    int node_id; // unique id for each node
+    int degree_of_separation; // how many nodes it took from the original infected node to infect it
+    char curr_state; // state of node: S, E, I, or R
+    float time_exposed; // time of exposure
+    float time_infect; // time of becoming infectious
+    float time_recovery; // time to recover
+    float infection_rate; // rate at which nodes get infected, S to E
+    float incubation_rate; // rate of E to I
+    float recovery_rate; // rate of I to R
+    vector<int> neighbors; // adjacency list of neighbor node ids
 
-private : int node_id;
-    int degree_of_separation;
-    char curr_state;
-    float time_exposed;
-    float time_infect;
-    float recovery_time;
-    float infection_rate;
-    vector<int> neighbors;
+public:
+    // constructor
+    Node(int id, char state = 'S', float infectionRate = 0.0, float incubationRate = 0.0, float recoveryRate = 0.0);
+
+    // getters
+    int getId();
+    int getDegree(); // num of immediate neighbors
+    int getDegreeOfSeparation(); // distance from a source
+    char getState();
+    float getTimeExposed();
+    float getTimeInfected();
+    float getTimeRecovered();
+    float getInfectionRate();
+    float getIncubationRate();
+    float getRecoveryRate();
+    vector<int> &getNeighbors(); // adjacency list
+
+    // setters
+    void setInfectionRate(float rate);
+    void setIncubationRate(float rate);
+    void setRecoveryRate(float rate);
+
+    // SEIR helpers
+    void markExposed(float time);
+    void markInfectious(float time);
+    void markRecovered(float time);
+
+    // markers
+    bool isSuseptible();
+    bool isExposed();
+    bool isInfectious();
+    bool isRecovered();
 };
