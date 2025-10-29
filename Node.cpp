01e4110148a2669448716fece3aa8769827465cc
uliannaa <152221245+ulyannaa@users.noreply.github.com>
2025-10-28 22:48:29 -0400

    Created Node.h file, created varibles

--- Node.cpp
+++ Node.cpp
@@ -1,5 +1,13 @@
+#pragma once
 #include <iostream>
 #include <vector>
 #include <unordered_map>
 #include <random>
 #include <queue>
+
+class Node {
+private:
+    int id = 0;
+    int state = 0; // state of node 0 - safe, 1 - exposed, 2 - infected, 3 - recovered
+
+};
---
+++ Node.h
@@ -0,0 +1,10 @@
+class Node {
+private:
+    int node_id;
+    int degree_of_separation;
+    char curr_state;
+    float time_exposed;
+    float time_infect;
+    float recovery_time;
+    float infection_rate;
+};
